SinixND/particles

feature(Shader) Add shader computation

assets/shaders/vertexShader.vert
@@ -1,47 +1,124 @@
-//* https://raw.githubusercontent.com/raysan5/raylib/master/examples/shaders/resources/shaders/glsl330/base.vs
-
-//* VERTEX SHADER
-//* := **Where** things are drawn
-//* Transforms verticies ("edges")
-
-//* VAO (vertex array object)
-//* Stores where to find the data for each input (vertex) attribute
-
-//* VBO (vertex buffer object)
-//* This is an object that holds/buffers verticies to be passed to the vertex shader
-
-//* VAO is needed BEFORE VBO!
-
-//* === SHADER START ===
-//* Required GLSL version
 #version 330 core
 
-//* Input vertex attributes
-//* in type nameIn;
-in vec3 vertexPosition;
-in vec2 vertexTexCoord;
-in vec3 vertexNormal;
-in vec4 vertexColor;
+layout(location = 0) in vec2 position;
+layout(location = 1) in vec2 velocity;
+layout(location = 2) in vec4 color;
 
-//* Input uniform (const?) values
-uniform mat4 mvp;
+layout(location = 3) uniform vec2 mousePosition;
+layout(location = 4) uniform float multiplier;
+layout(location = 5) uniform float friction;
+layout(location = 6) uniform float dt;
+layout(location = 7) uniform float screenWidth;
+layout(location = 8) uniform float screenHeight;
 
-//* Output vertex attributes (to fragment shader)
-//* out type nameOut;
-out vec2 fragTexCoord;
-out vec4 fragColor;
+out vec4 vertexColor;
 
-//* NOTE: Add your custom variables here
+Vec2 attract(
+    Vec2 velocity,
+    Vec2 target,
+    float multiplier
+);
+
+Vec2 applyFriction(
+    Vec2 velocity,
+    float value
+);
+
+Vec2 move(
+    Vec2 position,
+    Vec2 velocity,
+    float screenWidth,
+    float screenHeight,
+    float dt
+);
 
 void main()
 {
-    //* Output vertex attributes to fragment shader
-    fragTexCoord = vertexTexCoord;
-    fragColor = vertexColor;
+    velocity = attract(
+            velocity,
+            mousePosition,
+            multiplier
+        );
 
-    //* Reqired
-    //* Calculate final vertex position
-    gl_POSITION_ = mvp*vec4(vertexPosition, 1.0);
+    velocity = applyFriction(
+            velocity,
+            friction
+        );
+
+    position = move(
+            position,
+            velocity,
+            screenWidth,
+            screenHeight,
+            dt
+        );
+
+    gl_Position = vec4(position, 0.0, 1.0);
+
+    vertexColor = color;
 }
 
-// vim: set filetype=vert :
+Vec2 attract(
+    Vec2 velocity,
+    Vec2 target,
+    float multiplier
+)
+{
+    if (!target.x && !target.y)
+    {
+        return velocity;
+    }
+
+    Vec2 delta = target - position;
+
+    //* Distance influences acceleration, fmax caps velocity delta
+    float distance = max(length(delta), 100.0f);
+    Vector2 direction = Vector2Normalize(delta);
+
+    velocity = velocity + (direction * (multiplier / distance));
+
+    return velocity;
+}
+
+Vec2 applyFriction(
+    Vec2 velocity,
+    float value
+)
+{
+    velocity = velocity * value;
+
+    return velocity;
+}
+
+Vec2 move(
+    Vec2 position,
+    Vec2 velocity,
+    float screenWidth,
+    float screenHeight,
+    float dt
+)
+{
+    position = position + (velocity * dt);
+
+    if (position.x < 0)
+    {
+        position.x += screenWidth;
+    }
+
+    if (position.x > screenWidth)
+    {
+        position.x -= screenWidth;
+    }
+
+    if (position.y < 0)
+    {
+        position.y += screenHeight;
+    }
+
+    if (position.y > screenHeight)
+    {
+        position.y -= screenHeight;
+    }
+
+    return position;
+}

SinixND/ParticleComputeShader

assets/shaders/vertexShader.vert
@@ -1,47 +1,107 @@
-//* https://raw.githubusercontent.com/raysan5/raylib/master/examples/shaders/resources/shaders/glsl330/base.vs
-
-//* VERTEX SHADER
-//* := **Where** things are drawn
-//* Transforms verticies ("edges")
-
-//* VAO (vertex array object)
-//* Stores where to find the data for each input (vertex) attribute
-
-//* VBO (vertex buffer object)
-//* This is an object that holds/buffers verticies to be passed to the vertex shader
-
-//* VAO is needed BEFORE VBO!
-
-//* === SHADER START ===
-//* Required GLSL version
 #version 330 core
 
-//* Input vertex attributes
-//* in type nameIn;
-in vec3 vertexPosition;
-in vec2 vertexTexCoord;
-in vec3 vertexNormal;
-in vec4 vertexColor;
+layout(location = 0) in vec2 position;
+layout(location = 1) in vec2 velocity;
+layout(location = 2) in vec4 color;
 
-//* Input uniform (const?) values
-uniform mat4 mvp;
+uniform vec2 mousePosition;
+uniform float multiplier;
+uniform float friction;
+uniform float dt;
+uniform float screenWidth;
+uniform float screenHeight;
 
-//* Output vertex attributes (to fragment shader)
-//* out type nameOut;
-out vec2 fragTexCoord;
-out vec4 fragColor;
+out vec4 vertexColor;
 
-//* NOTE: Add your custom variables here
+vec2 attract(
+    vec2 velocity,
+    vec2 target,
+    float multiplier
+)
+{
+    if ((target.x == 0) && (target.y == 0))
+    {
+        return velocity;
+    }
+
+    vec2 delta = target - position;
+
+    //* Distance influences acceleration, fmax caps velocity delta
+    float distance = max(length(delta), 100.0f);
+    vec2 direction = normalize(delta);
+
+    velocity = velocity + (direction * (multiplier / distance));
+
+    return velocity;
+}
+
+vec2 applyFriction(
+    vec2 velocity,
+    float value
+)
+{
+    velocity = velocity * value;
+
+    return velocity;
+}
+
+vec2 move(
+    vec2 position,
+    vec2 velocity,
+    float screenWidth,
+    float screenHeight,
+    float dt
+)
+{
+    position = position + (velocity * dt);
+
+    if (position.x < 0)
+    {
+        position.x += screenWidth;
+    }
+
+    if (position.x > screenWidth)
+    {
+        position.x -= screenWidth;
+    }
+
+    if (position.y < 0)
+    {
+        position.y += screenHeight;
+    }
+
+    if (position.y > screenHeight)
+    {
+        position.y -= screenHeight;
+    }
+
+    return position;
+}
 
 void main()
 {
-    //* Output vertex attributes to fragment shader
-    fragTexCoord = vertexTexCoord;
-    fragColor = vertexColor;
+    vec2 newVelocity = attract(
+            velocity,
+            mousePosition,
+            multiplier
+        );
 
-    //* Reqired
-    //* Calculate final vertex position
-    gl_POSITION_ = mvp*vec4(vertexPosition, 1.0);
+    newVelocity = applyFriction(
+            newVelocity,
+            friction
+        );
+
+    vec2 newPosition = move(
+            position,
+            velocity,
+            screenWidth,
+            screenHeight,
+            dt
+        );
+
+    newPosition = normalize(newPosition);
+
+    gl_Position = vec4(newPosition, 0.0, 1.0);
+
+    vertexColor = color;
 }
-
-// vim: set filetype=vert :